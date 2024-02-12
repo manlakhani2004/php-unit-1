1a3c9933cc24d56bd1137af73680f00196032391
manlakhani2004 <126983888+manlakhani2004@users.noreply.github.com>
2024-02-12 23:40:02 +0530

    Add files via upload
    unit-2 folder upload

---
+++ unit-2/program1.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<html>
+<head>
+<title>Program 1</title>
+</head>
+<body>
+
+<form action="program1_1.php" method="POST">
+
+<label>enter your name:
+<input type='text' name="uname">
+</label>
+
+<label>enter your email:
+<input type="email" name="uemail">
+</label>
+
+<input type="submit" value="click me">
+
+</form>
+</body>
+</html>
---
+++ unit-2/program1_1.php
@@ -0,0 +1,10 @@
+<?php
+
+$fullname = $_POST["uname"];
+$email = $_POST["uemail"];
+
+echo "user submited data is: <br>";
+echo "full name is: $fullname <br>";
+echo "email id is: $email";
+
+?>
